dfbd710a789695f02d881d14d54357eb9a858747
Marcus-Vinicios <marcusv.fl.oliveira@gmail.com>
2023-11-25 20:28:49 -0300

    feat: structuring the HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { GrFormNext, GrFormPrevious } from "react-icons/gr"
+
 import './App.css'
 
 function App() {
 
   return (
     <div>
-      <h1>React Multistep</h1>
+      <div>
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
+      </div>
+      <div className="form-container">
+        etapas
+        <form>
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type='button'>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type='submit'>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
