c1ad65a6c814478257287da9c38f1f0f884e535f
Marcus-Vinicios <marcusv.fl.oliveira@gmail.com>
2023-11-25 22:59:56 -0300

    feat: creating the form components and a custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { GrFormNext, GrFormPrevious } from "react-icons/gr"
 
+import Thanks from "./components/Thanks/Thanks"
+import Review from "./components/ReviewForm/Review"
+import UserForm from "./components/UserForm/UserForm"
+
+// Hooks
+import { useForm } from "./hooks/useForm"
+
 import './App.css'
 
 function App() {
+  const components = [<UserForm />, <Review />, <Thanks />];
+  const { currentStep, currentComponent } = useForm(components);
 
   return (
     <div>
@@ -13,7 +22,9 @@ function App() {
       <div className="form-container">
         etapas
         <form>
-          <div className="inputs-container"></div>
+          <div className="inputs-container">
+            {currentComponent}
+          </div>
           <div className="actions">
             <button type='button'>
               <GrFormPrevious />
---
+++ src/components/Thanks/Thanks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Thanks() {
+  return (
+    <div>Thanks</div>
+  )
+}
+
+export default Thanks
